c7daeb7be2f376f4482a019060ae26c729ca615f
NguyenTuong <nguyenntuong@gmail.com>
2018-09-12 22:55:39 +0700

    ssss

--- client-sent.c
+++ client-sent.c
@@ -42,15 +42,14 @@ int main(int argc, char *argv[])
     serv_addr.sin_port = htons(portno);
     if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
         error("ERROR connecting");
-
+    bzero(buffer, 1024);
+    n = read(sockfd, buffer, 1024);
+    if (n < 0)
+        error("ERROR reading from socket");
+    if(n>0)
+        printf("%s\n", buffer);
     while (1)
-    {
-        bzero(buffer, 1024);
-        n = read(sockfd, buffer, 1024);
-        if (n < 0)
-            error("ERROR reading from socket");
-        if(n>0)
-            printf("%s\n", buffer);
+    {        
         printf("Please enter the message: ");
         bzero(buffer, 1024);
         fgets(buffer, 1024, stdin);
